f69f5824ca05dd974a46053ea51864f12ab0f237
Vic <vicxd@pm.me>
2025-09-16 22:34:52 +0200

    fix the button

--- scripts/marblebutton.gd
+++ scripts/marblebutton.gd
@@ -1,13 +1,36 @@
 extends Button
 @export var marble_scene: PackedScene
-func _pressed() -> void:
-	if marble_scene == null:
-		pass
-	
+
+
+var _duration_pressed = 0.0
+var _charging = false
+
+func _ready() -> void:
+	button_down.connect(_on_button_down)
+	button_up.connect(_on_button_up)
+
+
+func _process(delta: float) -> void:
+	if _charging:
+		_duration_pressed += delta
+
+
+func _on_button_down() -> void:
+	_charging = true
+	_duration_pressed = 0.0
+
+func _on_button_up() -> void:
+	_charging = false
 	var angle = deg_to_rad(-45)
 	var dir = Vector2.RIGHT.rotated(angle)
+	spawn_marble_with_v(Vector2(-475, -200.0), dir, 300.0 * (_duration_pressed+1))
+	print(_duration_pressed)
+	_duration_pressed = 0.0
+
+
 	
-	spawn_marble_with_v(Vector2(0,-80.0),dir,400.0)
+
+	
 	
 	
 	
