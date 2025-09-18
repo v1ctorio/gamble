7fca6515325400204e7f3744f80a2ac4b47e2e84
Vic <vicxd@pm.me>
2025-09-18 22:24:46 +0200

    add score pipes

---
+++ scenes/pachinko/score_pipe.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var giving_score: int = 0
+
+func _ready() -> void:
+	$Label.text = str(giving_score)
+	$Area2D.body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body) -> void:
+	if body is Marble:
+		dispatch_score()
+		
+func dispatch_score():
+	global_state.score += giving_score
+	print(global_state.score)
